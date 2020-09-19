f5e4568ec82b4f13561672a9d71e627fab27db2e
Martin Lalev <martin.laleff@gmail.com>
2020-09-19 10:06:26 +0100

    Initial Commit

---
+++ FlooidObservables/FlooidObservables.h
@@ -0,0 +1,18 @@
+//
+//  FlooidObservables.h
+//  FlooidObservables
+//
+//  Created by Martin Lalev on 19/09/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlooidObservables.
+FOUNDATION_EXPORT double FlooidObservablesVersionNumber;
+
+//! Project version string for FlooidObservables.
+FOUNDATION_EXPORT const unsigned char FlooidObservablesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidObservables/PublicHeader.h>
+
+
